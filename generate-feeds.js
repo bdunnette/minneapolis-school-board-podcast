888f64f874a29c5264260d84de662fcfe09e1411
Brian Dunnette <brian@dunnette.us>
2017-10-09 20:18:46 -0500

    Create generate-feeds.js

---
+++ generate-feeds.js
@@ -0,0 +1,19 @@
+var cheerio = require('cheerio'),
+    cheerioTableparser = require('cheerio-tableparser'),
+    rss = require('rss'),
+    request = require('request'),
+    _ = require('lodash'),
+    fs = require('fs'),
+    path = require('path');
+    
+var url = 'http://mplsk12mn.granicus.com/ViewPublisher.php?view_id=2'
+
+request.get({
+    url: url
+}, function(err, httpResponse, body) {
+    var $ = cheerio.load(body);
+    cheerioTableparser($);
+    var archive = $('table #archive');
+    tableData = archive.parsetable(true, true, true);
+    console.log(tableData);
+})
