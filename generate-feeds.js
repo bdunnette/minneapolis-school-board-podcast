12c2623d6e96b3349696e5335a7de1a4cd123375
Brian Dunnette <dunn0172@umn.edu>
2017-10-10 16:02:09 -0500

    Basic parsing of table - just need to generate XML

--- generate-feeds.js
+++ generate-feeds.js
@@ -5,17 +5,48 @@ var cheerio = require('cheerio'),
     rss = require('rss'),
     request = require('request'),
     _ = require('lodash'),
+    moment = require('moment'),
     fs = require('fs'),
     path = require('path');
-    
+
 var url = 'http://mplsk12mn.granicus.com/ViewPublisher.php?view_id=2'
+var feed = new rss({
+    title: "Minneapolis School Board Meetings",
+    pubDate: new Date()
+});
+
+console.log(feed);
 
 request.get({
     url: url
-}, function(err, httpResponse, body) {
+}, function (err, httpResponse, body) {
     var $ = cheerio.load(body);
     cheerioTableparser($);
     var archive = $('table #archive');
-    tableData = archive.parsetable(true, true, true);
-    console.log(tableData);
+    tableData = archive.parsetable(false, false, false);
+    // console.log(tableData);
+    var titles = tableData[0],
+        dates = tableData[1],
+        durations = tableData[2],
+        agendas = tableData[3],
+        video = tableData[5],
+        audio = tableData[6];
+    audio.forEach(function (element, index) {
+        console.log(index)
+        console.log(element);
+        if (element.length > 6) {
+            var dateString = $(dates[index]).text().substring(10)
+            console.log(dateString)
+            var date = moment(dateString, "MMM DD, YYYY").toDate()
+            var enclosure = $(element).attr('href');
+            console.log(enclosure)
+            feed.item({
+                title: titles[index],
+                date: date,
+                enclosure: {
+                    url: enclosure
+                }
+            })
+        }
+    });
 })
